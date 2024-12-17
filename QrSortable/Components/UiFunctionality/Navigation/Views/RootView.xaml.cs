aa3524ad028a9845c980e886a115f81ff425416a
Praveen Kumar Das <pdas@mpr-bayern.de>
2024-12-17 11:13:20 +0100

    # no loop in the CarouselView

--- QrSortable/Components/UiFunctionality/Navigation/Views/RootView.xaml.cs
+++ QrSortable/Components/UiFunctionality/Navigation/Views/RootView.xaml.cs
@@ -11,7 +11,7 @@
         ///  Initializes a new instance of the RootView class with the specified view model.
         /// </summary>
         /// <param name="viewModel">The RootViewModel associated with this view.</param>
-        public RootView(RootViewModel viewModel):base(viewModel)
+        public RootView(RootViewModel viewModel) : base(viewModel)
         {
             InitializeComponent();
             BindingContext = viewModel;
