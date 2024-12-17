3742f8f4304e08e2eddea1619632a9cd72a98a8d
Praveen Kumar Das <pdas@mpr-bayern.de>
2024-12-17 14:17:23 +0100

    #database first draft

--- QrSortable/Components/CoreFeatures/Scanner/ViewModels/BoxDetailViewModel.cs
+++ QrSortable/Components/CoreFeatures/Scanner/ViewModels/BoxDetailViewModel.cs
@@ -1,6 +1,9 @@
 ﻿namespace QrSortable.Components.CoreFeatures.Scanner.ViewModels
 {
+    using CommunityToolkit.Mvvm.ComponentModel;
     using CommunityToolkit.Mvvm.Input;
+    using QrSortable.Components.CoreFeatures.DataManagement.General;
+    using QrSortable.Components.CoreFeatures.DataManagement.General.Models;
     using QrSortable.Components.CoreFeatures.Scanner.Views;
     using QrSortable.Components.UiFunctionality.Navigation.ViewModels;
 
@@ -9,14 +12,18 @@
     /// </summary>
     public partial class BoxDetailViewModel : BaseViewModel<string>
     {
-        private string _barcode;
+        private readonly IDatabaseManager _databaseManager;
+        private StorageEntry _storageData = new StorageEntry();
 
         /// <summary>
         ///     Initializes a new instance of the <see cref="BoxDetailViewModel" />.
         /// </summary>
-        public BoxDetailViewModel()
+        /// <param name="databaseManager">An instance of <see cref="IDatabaseManager" /> 
+        /// used for managing database operations.</param>
+        public BoxDetailViewModel(IDatabaseManager databaseManager)
         {
             IsBackNavigationEnabled = true;
+            _databaseManager = databaseManager;
         }
 
         /// <summary>
@@ -27,7 +34,26 @@
         public override async Task InitializeAsync()
         {
             await base.InitializeAsync();
+
+            var storageArray = await _databaseManager.GetListAsync<StorageEntry>();
+
+            if(storageArray != null)
+            {
+                //populate the item name and item image from already stored data from database
+            }
         }
+
+        [ObservableProperty]
+        private string _barcode;
+
+        [ObservableProperty]
+        private string _barcodeType;
+
+        [ObservableProperty]
+        private string _location;
+
+        [ObservableProperty]
+        private string _category;
 
         /// <summary>
         ///     Prepares the viewmode with an barcode raw data.
@@ -35,13 +61,17 @@
         /// <param name="barcode">The string barcode data.</param>
         public override async void Prepare(string barcode)
         {
-            _barcode = barcode.Trim();
+            string[] result = barcode.Split(',');
+            Barcode = _storageData.BarcodeValue = result[0].Trim();
+            BarcodeType = _storageData.BarcodeType = result[1].Trim();
 
         }
 
         public AsyncRelayCommand AddItemCommand => new AsyncRelayCommand(async () =>
         {
-            await NavigationService.Navigate<ItemDetailView>();
+            _storageData.Location = Location;
+            _storageData.Category = Category;
+            await NavigationService.Navigate<ItemDetailView>(_storageData, false);
         });
 
     }
--- QrSortable/Components/CoreFeatures/Scanner/ViewModels/ItemDetailViewModel.cs
+++ QrSortable/Components/CoreFeatures/Scanner/ViewModels/ItemDetailViewModel.cs
@@ -1,19 +1,28 @@
 ﻿namespace QrSortable.Components.CoreFeatures.Scanner.ViewModels
 {
+    using CommunityToolkit.Mvvm.ComponentModel;
+    using CommunityToolkit.Mvvm.Input;
+    using QrSortable.Components.CoreFeatures.DataManagement.General;
+    using QrSortable.Components.CoreFeatures.DataManagement.General.Models;
     using QrSortable.Components.UiFunctionality.Navigation.ViewModels;
 
     /// <summary>
     ///     The view model of the ItemDetailView screen.
     /// </summary>
-    public partial class ItemDetailViewModel : BaseViewModel
+    public partial class ItemDetailViewModel : BaseViewModel<StorageEntry>
     {
-        
+        private readonly IDatabaseManager _databaseManager;
+        private StorageEntry _storageData;
+
         /// <summary>
         ///     Initializes a new instance of the <see cref="ItemDetailViewModel" />.
         /// </summary>
-        public ItemDetailViewModel()
+        /// <param name="databaseManager">An instance of <see cref="IDatabaseManager" /> 
+        /// used for managing database operations.</param>
+        public ItemDetailViewModel(IDatabaseManager databaseManager)
         {
             IsBackNavigationEnabled = true;
+            _databaseManager = databaseManager;
         }
 
         /// <summary>
@@ -25,6 +34,64 @@
         {
             await base.InitializeAsync();
         }
+
+        [ObservableProperty]
+        private string _itemName;
+
+        [ObservableProperty]
+        private string _itemDescription;
+
+        /// <summary>
+        ///     Prepares the viewmode with an storage raw data.
+        /// </summary>
+        /// <param name="storageData">The string storage data.</param>
+        public override async void Prepare(StorageEntry storageData)
+        {
+            _storageData = storageData;
+
+            if (_storageData != null) 
+            { 
+                //TODO: furhter implementaion
+            }
+            else
+            {
+                Console.WriteLine("Error:ItemDetailViewModel:Prepare: storageData is null");
+            }
+
+        }
+
+        public AsyncRelayCommand AddItemCommand => new AsyncRelayCommand(async () =>
+        {
+
+        });
+
+        public AsyncRelayCommand SaveCommand => new AsyncRelayCommand(async () =>
+        {
+
+            if (_storageData != null)
+            {
+                _storageData.CreatedDate = DateTime.Now;
+
+
+                //_databaseManager.BeginTransaction();
+
+                //var wasAddingItemSuccessful = await _databaseManager.AddAsync(_storageData);
+
+                //if (wasAddingItemSuccessful != null)
+                //{
+                //    _databaseManager.CommitTransaction();
+                //    Console.WriteLine("Error:ItemDetailViewModel:SaveCommand: storageData is Saved");
+                //}
+                //else
+                //{
+                //    _databaseManager.Rollback();
+                //}
+            }
+            else
+            {
+                Console.WriteLine("Error:ItemDetailViewModel:SaveCommand: storageData is null");
+            }
+        });
 
     }
 }
