775ab6d48e6e59c90b6ebb2d603abd83036ab226
Praveen Kumar Das <pdas@mpr-bayern.de>
2025-11-28 13:17:53 +0100

    #created yours order view and viewmodels

---
+++ QrSortable/Components/CoreFeatures/OrdersPayments/ViewModels/YoursOrdersViewModel.cs
@@ -0,0 +1,49 @@
+﻿namespace QrSortable.Components.CoreFeatures.OrdersPayments.ViewModels
+{
+    using CommunityToolkit.Mvvm.Input;
+    using QrSortable.Components.UiFunctionality.Navigation.ViewModels;
+
+    /// <summary>
+    ///     The view model of the YoursOrdersViewModel screen.
+    /// </summary>
+    public partial class YoursOrdersViewModel : BaseViewModel
+    {
+
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="YoursOrdersViewModel" />.
+        /// </summary>
+        public YoursOrdersViewModel()
+        {
+            IsBackNavigationEnabled = true;
+           
+        }
+
+        /// <summary>
+        /// Initializes the component asynchronously, ensuring proper initialization of general information
+        /// and notification permissions.
+        /// </summary>
+        /// <returns>An awaitable task.</returns>
+        public override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+            
+        }
+
+
+        public AsyncRelayCommand SelectQrCommand => new AsyncRelayCommand(async () =>
+        {
+
+          
+
+        });
+
+        public AsyncRelayCommand SelectBarcodeCommand => new AsyncRelayCommand(async () =>
+        {
+
+
+
+        });
+
+    }
+}
---
+++ QrSortable/Components/CoreFeatures/OrdersPayments/Views/YoursOrdersView.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace QrSortable.Components.CoreFeatures.OrdersPayments.Views
+{
+    using QrSortable.Components.UiFunctionality.Navigation.Views;
+    using ViewModels;
+
+    /// <summary>
+    /// The code behind of the YoursOrdersView.
+    /// </summary>
+    public partial class YoursOrdersView : BaseView
+    {
+
+        /// <summary>
+        ///  Initializes a new instance of the YoursOrdersViewModel class with the specified view model.
+        /// </summary>
+        /// <param name="viewModel">The YoursOrdersViewModel associated with this view.</param>
+        public YoursOrdersView(YoursOrdersViewModel viewModel) : base(viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+        }
+    }
+}
