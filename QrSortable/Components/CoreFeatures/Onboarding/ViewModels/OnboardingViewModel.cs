e3092740cec3c457daff8acc1367bb6bb8fc6991
Praveen Kumar Das <pdas@mpr-bayern.de>
2025-12-15 11:50:51 +0100

    added ui for the multiuser id

--- QrSortable/Components/CoreFeatures/Onboarding/ViewModels/OnboardingViewModel.cs
+++ QrSortable/Components/CoreFeatures/Onboarding/ViewModels/OnboardingViewModel.cs
@@ -1,8 +1,11 @@
 ﻿namespace QrSortable.Components.CoreFeatures.Onboarding.ViewModels
 {
     using CommunityToolkit.Mvvm.ComponentModel;
+    using CommunityToolkit.Mvvm.Input;
+    using PdfSharpCore.Drawing.BarCodes;
     using QrSortable.Components.CoreFeatures.DataManagement.General;
     using QrSortable.Components.UiFunctionality.Navigation.ViewModels;
+    using QrSortable.Components.UiFunctionality.Notification;
 
 
     /// <summary>
@@ -11,12 +14,15 @@
     public partial class OnboardingViewModel : BaseViewModel
     {
         private readonly IGeneralInformationManager _generalInformationManager;
+        private readonly IToastService _toastService;
+
         /// <summary>
         ///     Initializes a new instance of the <see cref="OnboardingViewModel" />.
         /// </summary>
-        public OnboardingViewModel(IGeneralInformationManager generalInformationManager)
+        public OnboardingViewModel(IGeneralInformationManager generalInformationManager, IToastService toastService)
         {
             IsBackNavigationEnabled = true;
+            _toastService = toastService;
             _generalInformationManager = generalInformationManager;
         }
 
@@ -26,6 +32,13 @@
 
             MultiuserId = (await _generalInformationManager.GetGeneralInformationAsync()).MultiUserId;
         }
+
+        public AsyncRelayCommand CopyMultiuserCodeCommand => new AsyncRelayCommand(async () =>
+        {
+            await Clipboard.Default.SetTextAsync(MultiuserId);
+            await _toastService.DisplayToast("MultiuserId copied to clipboard!");
+
+        });
 
         /// <summary>
         /// Represents the currently multiuser identification in the application.
