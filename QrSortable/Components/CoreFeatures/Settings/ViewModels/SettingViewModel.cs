9684cdb4d90efd929843e59c55bc34df53019490
Praveen Kumar Das <pdas@mpr-bayern.de>
2025-12-22 08:11:59 +0100

    #users

--- QrSortable/Components/CoreFeatures/Settings/ViewModels/SettingViewModel.cs
+++ QrSortable/Components/CoreFeatures/Settings/ViewModels/SettingViewModel.cs
@@ -3,6 +3,8 @@
     using CommunityToolkit.Mvvm.Input;
     using QrSortable.Components.CoreFeatures.Cloud.BackendCommunication;
     using QrSortable.Components.CoreFeatures.Cloud.BackendCommunication.Models;
+    using QrSortable.Components.CoreFeatures.DataManagement.Backend;
+    using QrSortable.Components.CoreFeatures.DataManagement.Backend.Models;
     using QrSortable.Components.UiFunctionality.Navigation.ViewModels;
     using System.Threading.Tasks;
 
@@ -17,32 +19,64 @@
 
         private readonly IBackendCommunicationService _backendCommunicationService;
 
+        private readonly IBackendDatabaseManager _backendDatabaseManager;
+
+        private readonly IBackendSynchronizationManager _backendSynchronizationManager;
+
         /// <summary>
         ///     Initializes a new instance of the <see cref="SettingViewModel" />.
         /// </summary>
-        public SettingViewModel(IGeneralDatabaseSynchronizationManager generalDatabaseSynchronizationManager, IBackendCommunicationService backendCommunicationService)
+        public SettingViewModel(IGeneralDatabaseSynchronizationManager generalDatabaseSynchronizationManager,
+            IBackendCommunicationService backendCommunicationService, IBackendDatabaseManager backendDatabaseManager,
+            IBackendSynchronizationManager backendSynchronizationManager)
         {
             IsBackNavigationEnabled = true;
             _generalDatabaseSynchronizationManager = generalDatabaseSynchronizationManager;
             _backendCommunicationService = backendCommunicationService;
+            _backendDatabaseManager = backendDatabaseManager;
+            _backendSynchronizationManager = backendSynchronizationManager;
         }
 
 
-        public override Task InitializeAsync()
+        public async override Task InitializeAsync()
         {
-            return base.InitializeAsync();
+            await base.InitializeAsync();
+
+            //var dbEntries = await _backendDatabaseManager
+            //  .GetAllAsync<DtoStorageEntryModel>();
+            //dbEntries = dbEntries.OrderBy(dto => dto.ID);
+
+            //foreach (var dto in dbEntries)
+            //{
+            //    var entry = dto.Category;
+            //}
+
+            //var dbEntries1 = await _backendDatabaseManager
+            //  .GetAllAsync<DtoOrdersModel>();
+            //dbEntries1 = dbEntries1.OrderBy(dto => dto.ID);
+
+            //foreach (var dto in dbEntries1)
+            //{
+            //    var entry = dto.City;
+            //}
+
+            await _backendSynchronizationManager.SynchronizeStoredObjectsAsync();
         }
 
         public async override void ViewAppearing()
         {
             base.ViewAppearing();
 
-            var entries = await _backendCommunicationService.GetAllAsync<StorageEntryModel>();
-            for (int i = 0; i < entries.Count; i++)
-            {
-                var entry = entries[i];
-            }
+            //var entries = await _backendCommunicationService.GetAllAsync<StorageEntryModel>();
+            //for (int i = 0; i < entries.Count; i++)
+            //{
+            //    var entry = entries[i];
+            //}
+
+
+          
         }
+
 
         public AsyncRelayCommand SyncDataCommand => new AsyncRelayCommand(async () =>
         {
