8c884a3b960334a976882524c871e8c878ad09f4
praveen9345 <36010044+praveen9345@users.noreply.github.com>
2026-02-10 11:26:29 +0100

    Delete QrSortable/Components/CoreFeatures/Cloud/AcessManagement directory

--- QrSortable/Components/CoreFeatures/Cloud/AcessManagement/FirebaseConfig.cs
+++
@@ -1,9 +0,0 @@
-namespace QrSortable.Components.CoreFeatures.Cloud.AccessManagement
-{
-    public static class FirebaseConfig
-    {
-        public const string PROJECT_ID = "qrsortable-20c2d";
-    }
-
-}
-
