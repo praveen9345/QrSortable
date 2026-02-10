5018f8109ddae23a737cf770f26b6955a6c4b0d5
praveen9345 <36010044+praveen9345@users.noreply.github.com>
2026-02-10 11:14:48 +0100

    Fix namespace spelling in FirebaseConfig.cs

--- QrSortable/Components/CoreFeatures/Cloud/AcessManagement/FirebaseConfig.cs
+++ QrSortable/Components/CoreFeatures/Cloud/AcessManagement/FirebaseConfig.cs
@@ -1,4 +1,4 @@
-namespace QrSortable.Components.CoreFeatures.Cloud.AcessManagement
+namespace QrSortable.Components.CoreFeatures.Cloud.AccessManagement
 {
     public static class FirebaseConfig
     {
@@ -6,3 +6,4 @@ namespace QrSortable.Components.CoreFeatures.Cloud.AcessManagement
     }
 
 }
+
