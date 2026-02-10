bfba54211cf2ab3e7f93827b6567da58835370cc
praveen9345 <36010044+praveen9345@users.noreply.github.com>
2026-02-10 11:18:11 +0100

    added accessmangaement

---
+++ QrSortable/Components/CoreFeatures/Cloud/AccessManagement/FirebaseConfig.cs
@@ -0,0 +1,7 @@
+namespace QrSortable.Components.CoreFeatures.Cloud.AccessManagement
+{
+    public static class FirebaseConfig
+    {
+        public const string PROJECT_ID = "qrsortable-20c2d";
+    }
+}
