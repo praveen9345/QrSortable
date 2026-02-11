64a9e6bb725c2f50cae096fc48d580ba232ab8ef
praveen <praveen9345@gmail.com>
2026-02-11 21:47:27 +0100

    #mollie 1

--- QrSortable/Platforms/iOS/AppDelegate.cs
+++ QrSortable/Platforms/iOS/AppDelegate.cs
@@ -5,6 +5,7 @@ using Foundation;
 using UIKit;
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
+using QrSortable.Components.CoreFeatures.OrdersPayments.ViewModels;
 
 namespace QrSortable;
 
@@ -38,6 +39,30 @@ public class AppDelegate : MauiUIApplicationDelegate
         };
 
         return base.FinishedLaunching(app, options);
+    }
+
+    public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+    {
+        if (url != null)
+        {
+            var uri = new Uri(url.AbsoluteString);
+
+            // Parse the query: myapp://payment-return?id=xxx
+            var query = System.Web.HttpUtility.ParseQueryString(uri.Query);
+            var paymentId = query.Get("id");
+
+            if (!string.IsNullOrEmpty(paymentId))
+            {
+                // Get MainPage's BindingContext (ViewModel)
+                if (Microsoft.Maui.Controls.Application.Current.MainPage?.BindingContext
+                    is PaymentShipmentViewModel vm)
+                {
+                    vm.HandleMollieRedirect(paymentId);
+                }
+            }
+        }
+
+        return true;
     }
 
     private void CreateDirectoriesAndSetSkipBackupAttribute()
