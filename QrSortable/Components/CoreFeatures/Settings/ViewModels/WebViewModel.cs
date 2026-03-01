cc9fdaac171e128d85654b30e3670fc95eb4d70c
Praveen Kumar Das <pdas@mpr-bayern.de>
2026-03-01 12:07:30 +0100

    #adjusted the feedback url based on salected language

--- QrSortable/Components/CoreFeatures/Settings/ViewModels/WebViewModel.cs
+++ QrSortable/Components/CoreFeatures/Settings/ViewModels/WebViewModel.cs
@@ -2,9 +2,11 @@
 {
 
     using CommunityToolkit.Mvvm.ComponentModel;
+    using QrSortable.Components.CoreFeatures.DataManagement.General;
     using QrSortable.Components.PlatformUtils.Wrappers;
     using QrSortable.Components.UiFunctionality.Localization;
     using UiFunctionality.Navigation.ViewModels;
+    using static QRCoder.PayloadGenerator;
 
     /// <summary>
     ///     The view model of the feedback screen.
@@ -12,14 +14,17 @@
     public partial class WebViewModel : BaseViewModel<string>
     {
         private readonly IMauiEssentialsWrapper _mauiEssentialWrapper;
+        private readonly IGeneralInformationManager _generalInformationManager;
   
         /// <summary>
         ///     Initializes a new instance of the <see cref="WebViewModel" />.
         /// </summary>
         /// <param name="mauiEssentialsWrapper">An instance of <see cref="IMauiEssentialsWrapper" /> used to access platform-specific features.</param>
-        public WebViewModel(IMauiEssentialsWrapper mauiEssentialsWrapper)
+        public WebViewModel(IMauiEssentialsWrapper mauiEssentialsWrapper, IGeneralInformationManager generalInformationManager)
         {
             _mauiEssentialWrapper = mauiEssentialsWrapper;
+            _generalInformationManager = generalInformationManager;
+
             IsBackNavigationEnabled = true;
         }
 
@@ -43,7 +48,15 @@
             switch(viewName)
             {
                 case "feedback":
-                    Url = "https://forms.gle/5Cx6NTnz7boGvLUK8"; //english feedback
+                    var langCode = (await _generalInformationManager.GetGeneralInformationAsync()).SelectedLanguageCode;
+                    
+                    Url = langCode switch
+                    {
+                        "de" => "https://forms.gle/FSoQkoajwYNASEMq8",// german
+                        "es" => "https://forms.gle/aiHxRbrnfugz2ddKA",// spanish
+                        "fr" => "https://forms.gle/GzjQw6CyMhExqkWg6",// french
+                        _ => "https://forms.gle/5Cx6NTnz7boGvLUK8" // default: English
+                    };
                     break;
                 case "privacyPolicy":
                     Url = "https://kidsytales.com/privacy-policy";
