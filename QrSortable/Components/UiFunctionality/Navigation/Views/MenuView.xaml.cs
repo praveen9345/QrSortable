af72fe95bd096854163b3f2883f084052b312b73
Praveen Kumar Das <pdas@mpr-bayern.de>
2026-03-03 18:55:01 +0100

    #menu effect after click

--- QrSortable/Components/UiFunctionality/Navigation/Views/MenuView.xaml.cs
+++ QrSortable/Components/UiFunctionality/Navigation/Views/MenuView.xaml.cs
@@ -7,6 +7,7 @@
     /// </summary>
     public partial class MenuView : BaseView
     {
+        private readonly MenuViewModel _viewModel;
 
         /// <summary>
         ///  Initializes a new instance of the MenuViewModel class with the specified view model.
@@ -15,7 +16,25 @@
         public MenuView(MenuViewModel viewModel) : base(viewModel)
         {
             InitializeComponent();
-            BindingContext = viewModel;
+            BindingContext = _viewModel = viewModel;
+        }
+
+        private async void OnMenuItemTapped(object sender, TappedEventArgs e)
+        {
+            if (sender is Grid grid && grid.BindingContext is 
+                Models.MenuItem menuItem)
+            {
+                // Animation
+                grid.BackgroundColor = Color.FromArgb("#33ffffff");
+                await grid.ScaleTo(0.96, 80, Easing.CubicOut);
+                await grid.ScaleTo(1.0, 80, Easing.CubicIn);
+                grid.BackgroundColor = Colors.Transparent;
+
+                // Trigger navigation command
+
+                if (_viewModel?.OnSelectionMenuItemChangedCommand.CanExecute(menuItem) == true)
+                    await _viewModel.OnSelectionMenuItemChangedCommand.ExecuteAsync(menuItem);
+            }
         }
     }
 }
