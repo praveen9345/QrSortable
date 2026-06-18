8c4fb79f70601caf16913abcb88dae561f31ebcc
Praveen Kumar Das <pdas@mpr-bayern.de>
2026-06-18 12:06:25 +0200

    #correction fro the status bar

--- QrSortable/MainPage.xaml.cs
+++ QrSortable/MainPage.xaml.cs
@@ -4,13 +4,32 @@
     using QrSortable.Components.PlatformUtils;
     using QrSortable.Components.UiFunctionality.Navigation;
     using QrSortable.Components.UiFunctionality.Navigation.Views;
+    using Microsoft.Maui.Controls;
+
+#if ANDROID
+    using Microsoft.Maui.ApplicationModel;
+#endif
+
+#if IOS
+    using UIKit;
+#endif
+
 
     public partial class MainPage : ContentPage
     {
 	    public MainPage()
 	    {
 		    InitializeComponent();
-	    }
+
+#if ANDROID
+            var activity = Platform.CurrentActivity;
+            activity?.Window?.SetStatusBarColor(Android.Graphics.Color.ParseColor("#525252"));
+#endif
+
+#if IOS
+        UIKit.UIApplication.SharedApplication.StatusBarStyle = UIKit.UIStatusBarStyle.LightContent;
+#endif
+        }
 
         private async void OnClicked(object sender, EventArgs e)
         {
