a1511f1bb01b967850f186aac3e8b5600698c14c
Praveen Kumar Das <pdas@mpr-bayern.de>
2026-06-18 13:09:15 +0200

    #correction of bool converter for iOS simulation

---
+++ QrSortable/Components/UiFunctionality/Navigation/Converter/BoolInverterConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace QrSortable.Components.UiFunctionality.Navigation.Converter
+{
+    using System;
+    using System.Globalization;
+    using Microsoft.Maui.Controls;
+
+
+    public class BoolInverterConverter : IValueConverter
+    {
+        // ViewModel → UI
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool booleanValue)
+                return !booleanValue;
+
+            return value;
+        }
+
+        // UI → ViewModel (optional)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool booleanValue)
+                return !booleanValue;
+
+            return value;
+        }
+    }
+}
